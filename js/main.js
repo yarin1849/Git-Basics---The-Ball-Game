693e61238524573085897e0cdc0aa8c1a0254730
Yarin Bracha <yarin1849@gmail.com>
2024-12-24 16:19:55 +0200

    Update onBallClick func

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 'use strict'
 
 function onBallClick() {
-
-    console.log("Ball clicked");
+    var elBall = document.querySelector('.ball')
+    elBall.style.height = 150 + 'px'
+    elBall.style.width = 150 + 'px'
+    elBall.innerHTML = '150'
 }
