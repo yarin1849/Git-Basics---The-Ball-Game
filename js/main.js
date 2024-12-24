ce9a93a80851401ceff6d8ebaa74ebbf71c954c5
Yarin Bracha <yarin1849@gmail.com>
2024-12-24 18:11:04 +0200

    Added maxDiameter parameter

--- js/main.js
+++ js/main.js
@@ -4,15 +4,21 @@ const ball1 = {
     size: 100
 }
 
-function onBallClick(elBall) {
-    ball1.size += getRandomInt(20, 60)
+function onBallClick(elBall, maxDiameter) {
+    if (ball1.size < maxDiameter) {
 
-    elBall.style.backgroundColor = getRandomColor()
+        ball1.size += getRandomInt(20, 60)
 
-    elBall.style.width = ball1.size + 'px'
-    elBall.style.height = ball1.size + 'px'
-    elBall.innerText = ball1.size
+        elBall.style.backgroundColor = getRandomColor()
 
-    if (ball1.size >= 400) ball1.size = 50
+        elBall.style.width = ball1.size + 'px'
+        elBall.style.height = ball1.size + 'px'
+        elBall.innerText = ball1.size
+    }
+    else {
+        ball1.size = 100
+    }
+
+    // if (ball1.size >= maxDiameter) ball1.size = 50
 }
 
